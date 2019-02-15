c732bd7ba1b0fb6bde5f424c5522fe7b2839705a
Yugal+1-at-888520271636 <1DyFkjKXZiWvvt0SpZRE8rvOClJnGfesqnCEh3YgNVo=>
2019-02-15 13:41:33 +0530

    springboot empty skeleton

---
+++ src/main/java/com/stocker/stocker/StockerApplication.java
@@ -0,0 +1,14 @@
+package com.stocker.stocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/stocker/stocker/StockerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.stocker.stocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
