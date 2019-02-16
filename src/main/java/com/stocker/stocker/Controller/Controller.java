fb59c2a4689c6c86cbb79e8f0cbf88fad138da8e
Yugal+1-at-888520271636 <1DyFkjKXZiWvvt0SpZRE8rvOClJnGfesqnCEh3YgNVo=>
2019-02-16 14:39:37 +0530

    API to show transactions added

--- src/main/java/com/stocker/stocker/Controller/Controller.java
+++ src/main/java/com/stocker/stocker/Controller/Controller.java
@@ -1,8 +1,16 @@
 package com.stocker.stocker.Controller;
 
+import com.stocker.stocker.Models.Transactions;
+import com.stocker.stocker.Repositories.BuyOrders;
+import com.stocker.stocker.Repositories.SellOrders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
+import java.util.List;
+
 
 
 @CrossOrigin(origins = "*",maxAge = 3600)
@@ -10,6 +18,19 @@ import java.io.Serializable;
 @RequestMapping("/api")
 
 public class Controller implements Serializable {
+    @Autowired
+    BuyOrders buyOrders;
+
+    @Autowired
+    SellOrders sellOrders;
+
+    @Autowired
+    com.stocker.stocker.Repositories.Transactions transaction;
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<Transactions>> getAllClassUrl() {
+        return new ResponseEntity<List<Transactions>>(transaction.findAll(), HttpStatus.OK);
+    }
 
 
 
