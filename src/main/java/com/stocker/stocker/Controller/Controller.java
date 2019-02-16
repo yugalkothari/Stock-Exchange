6692a99a8ab725964c1c380f02a78322df955442
Yugal+1-at-888520271636 <1DyFkjKXZiWvvt0SpZRE8rvOClJnGfesqnCEh3YgNVo=>
2019-02-16 14:49:23 +0530

    API for adding orders

--- src/main/java/com/stocker/stocker/Controller/Controller.java
+++ src/main/java/com/stocker/stocker/Controller/Controller.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
+import java.sql.Time;
 import java.util.List;
 
 
@@ -28,11 +29,15 @@ public class Controller implements Serializable {
     com.stocker.stocker.Repositories.Transactions transaction;
 
     @GetMapping("/transactions")
-    public ResponseEntity<List<Transactions>> getAllClassUrl() {
+    public ResponseEntity<List<Transactions>> getAllTransactions() {
         return new ResponseEntity<List<Transactions>>(transaction.findAll(), HttpStatus.OK);
     }
 
+    @PostMapping("/addorder")
+    public void addTransactions(@RequestParam(value = "type",required = true) String type,@RequestParam(value = "price",required = true) Float price,@RequestParam(value = "quantity",required = true) Long quantity,@RequestParam(value = "transactionid",required = true) Long transactionid,@RequestParam(value = "stock",required = true) String stock,@RequestParam(value = "processed",required = true) int processed,@RequestParam(value = "time",required = true) Time time) {
 
+
+    }
 
 }
 
